8dcc695d59e2cb43c53b9ab97d88522e5630c481
Shinmen Dokkodo <shinmen.dokkodo@gmail.com>
2024-02-22 16:50:26 +0530

    added mapping profiles

--- CompanyEmployees/MappingProfile.cs
+++ CompanyEmployees/MappingProfile.cs
@@ -6,7 +6,16 @@ namespace CompanyEmployees;
 
 public class MappingProfile : Profile
 {
-    public MappingProfile() { CreateMap<Company, CompanyDto>()
-        .ForCtorParam("FullAddress", opt => 
-            opt.MapFrom(x => string.Join(' ', x.Address, x.Country))); }
+    public MappingProfile() 
+    { 
+        CreateMap<Company, CompanyDto>()
+            .ForMember(companyDto => companyDto.FullAddress, configurationExpression => 
+                configurationExpression.MapFrom(x => string.Join(' ', x.Address, x.Country))); 
+        
+        CreateMap<Employee, EmployeeDto>();
+        
+        CreateMap<CompanyForCreationDto, Company>();
+
+        CreateMap<EmployeeForCreationDto, Employee>();
+    }
 }
