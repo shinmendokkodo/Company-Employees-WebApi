0f849ec2fc5e1561d318dfd0df4845b74c2fc548
Shinmen Dokkodo <shinmen.dokkodo@gmail.com>
2024-02-22 16:51:47 +0530

    update logging

--- Contracts/ILoggerManager.cs
+++ Contracts/ILoggerManager.cs
@@ -3,7 +3,11 @@
 public interface ILoggerManager
 {
     void LogInfo(string message);
+    void LogInfo<T>(T input);
     void LogWarn(string message);
+    void LogWarn<T>(T input);
     void LogDebug(string message);
+    void LogDebug<T>(T input);
     void LogError(string message);
+    void LogError<T>(T input);
 }
--- LoggerService/LoggerManager.cs
+++ LoggerService/LoggerManager.cs
@@ -1,21 +1,49 @@
-﻿using Contracts;
+﻿using System.Text.Json;
+using Contracts;
 using NLog;
 
 namespace LoggerService;
 
 public class LoggerManager : ILoggerManager
 {
-    private static readonly ILogger Logger = LogManager.GetCurrentClassLogger();
+    private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
 
-    public LoggerManager()
+    public LoggerManager() { }
+
+    public void LogDebug(string message) => Logger.Debug(message);
+
+    public void LogDebug<T>(T input)
     {
+        string json = JsonSerialize(input);
+        Logger.Debug(json);
+    }
+
+    public void LogError(string message) => Logger.Error(message);
+
+    public void LogError<T>(T input)
+    {
+        string json = JsonSerialize(input);
+        Logger.Error(json);
     }
 
     public void LogInfo(string message) => Logger.Info(message);
 
-    public void LogDebug(string message) => Logger.Debug(message);
-
-    public void LogError(string message) => Logger.Error(message);
+    public void LogInfo<T>(T input)
+    {
+        string json = JsonSerialize(input);
+        Logger.Info(json);
+    }
 
     public void LogWarn(string message) => Logger.Warn(message);
+
+    public void LogWarn<T>(T input)
+    {
+        string json = JsonSerialize(input);
+        Logger.Warn(json);
+    }
+
+    private static string JsonSerialize<T>(T input)
+    {
+        return JsonSerializer.Serialize(input);
+    }
 }
